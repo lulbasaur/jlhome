2acf8631561ceac278c0bf2fdae3e14874357302
Lukas Hamberg <lukas.hamberg@gmail.com>
2018-07-20 08:19:56 +0200

    added sectionbox

---
+++ src/components/SectionBox.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="sectionbox">
+  <vs-card vs-color="primary">
+    <vs-card-header
+        vs-background-color="primary"
+        :vs-title=title
+        :vs-subtitle=subtitle>
+    </vs-card-header>
+    <vs-card-body>
+      <span>{{ body }}</span>
+    </vs-card-body>
+  </vs-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionBox',
+  props: {
+    title: String,
+    subtitle: String,
+    body: String,
+    mediaURL: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.cardbox {
+  height: 20%;
+}
+</style>
